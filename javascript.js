81e465b3a780a1d3dea0bad3c228559c438fcca1
andreia <108195837+andreiacrodrigues@users.noreply.github.com>
2023-11-13 02:03:08 +0000

    new html, css and js

--- javascript.js
+++ javascript.js
@@ -1,25 +1,38 @@
 let eachRound = document.querySelector('.round')
 let total = document.querySelector('.final')
+let buttons = document.querySelectorAll('button')
+let totalUser = document.querySelector('#pointUser')
+let totalComputer = document.querySelector('#pointComputer')
 
+let round = 5
+let playerScore = 0
+let computerScore = 0
 let resultRound
 
-
 function getPlayerChoice(){
-    let playerSelection = prompt("Choose between Rock, Paper, or Scissors").toLowerCase() //question to player
-
-    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors' && playerSelection !== ''){ //just accept 3 options
-
-        alert(`You choose ${playerSelection}`)
-        
-    } else{
-        alert(`You can't choose that... you need to choose one of them (Rock, Paper, or Scissors)`) 
-    }
-    return playerSelection
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+           if (round >= 0){
+            playRound(button.id, getComputerChoice())
+            --round
+           } else {
+                if (playerScore> computerScore){
+                    total.textContent = "Player WINS!!!"
+                    total.style.backgroundColor = 'green'
+                } else if (playerScore < computerScore){
+                    total.textContent = "Player LOST!"
+                    total.style.backgroundColor = 'red'
+                } else{
+                    total.textContent = "It's a TIE!"
+                }
+           }
+            
+        })
+    })
 }
+getPlayerChoice()
 
-playerSelection = getPlayerChoice() //answer of player
-
-function getComputerChoice(){ //computer choose one of 3 options
+function getComputerChoice(){ 
     const randomNumber = Math.random().toFixed(2)
     if(randomNumber < 0.33){
         return "rock"
@@ -30,59 +43,22 @@ function getComputerChoice(){ //computer choose one of 3 options
     }
 }
 
-let computerSelection = getComputerChoice() //answer of computer
-
-
-function playRound(playerSelection, computerSelection){ //player + computer = who wins?
-
-    if(playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection ==='paper' || playerSelection === 'paper' && computerSelection ==='rock'){
-        eachRound.innerHTML += `You Win! The ${playerSelection} beats ${computerSelection}!<br>` 
+function playRound(playerSelection, computerSelection){ 
+    if(playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection ==='paper' || playerSelection === 'paper' && computerSelection ==='rock' ){
+        total.textContent = `You Win! The ${playerSelection} beats ${computerSelection}!` 
+        playerScore ++
+        totalUser.textContent = playerScore 
         return resultRound = 'winner'
     
     } else if (playerSelection === computerSelection) {
-        eachRound.innerHTML += `It's a tie! You choose ${playerSelection} and computer choose ${computerSelection}!<br>`
+        total.textContent = `It's a tie! You choose ${playerSelection} and computer choose ${computerSelection}!`
         return resultRound = 'tie'
 
     } else{
-        eachRound.innerHTML+= `You Lose! The ${playerSelection} beats ${computerSelection}!<br>` 
+        total.textContent= `You Lose! The ${playerSelection} beats ${computerSelection}!`
+        computerScore++
+        totalComputer.textContent = computerScore
         return resultRound = 'loser'
     }
     
 }
-
-
-function game(){  //repeat 5 times and show a result
-
-    let playerScore = 0
-    let computerScore = 0
-    let results = []
-
-
-    for(let i=1; i<=5; i++){ //repeat 5 times and have a result for each round
-        playerSelection = getPlayerChoice()
-        computerSelection = getComputerChoice()
-        resultRound = playRound(playerSelection, computerSelection) 
-        results.push(resultRound)
-
-        if (resultRound === "winner"){ //refresh scores
-            resultRound === 1
-            playerScore++
-
-        } else if (resultRound === "loser") {
-            resultRound === -1
-            computerScore++
-        }
-    }
-
-    if(playerScore > computerScore){ //show final result
-        total.textContent += "Player WINS!"
-    } else if (playerScore < computerScore){
-        total.textContent += "Player LOST!"
-    } else{
-        total.textContent += "It's a TIE!"
-    }
-}
-game()
-
-
-
